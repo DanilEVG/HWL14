2440985aa49261431662e0ede1c677e93141a111
DanilEVG <89922067067@mail.ru>
2024-06-19 21:22:35 +0500

    HWL14

--- HWL14/ViewController.swift
+++ HWL14/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChangeFrame(_:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 
-
+    @IBAction func tapGestureHandler(_ sender: UITapGestureRecognizer) {
+        guard sender.state == .ended else { return }
+        view.endEditing(false)
+    }
+    
+    
+    @IBOutlet var stackViewHeightConstr: [NSLayoutConstraint]!
+    
+    @objc func keyboardWillChangeFrame(_ notification: Notification) {
+        
+        guard let frame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+        print("keyboard ")
+        
+        let keyboardTop = view.frame.height - view .convert(frame, from: nil).minY
+        scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardTop, right: 0)
+        scrollView.scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardTop - scrollView.safeAreaInsets.bottom , right: 0)
+        if keyboardTop > 0 {
+            NSLayoutConstraint.deactivate(stackViewHeightConstr)
+        } else {
+            NSLayoutConstraint.activate(stackViewHeightConstr)
+        }
+    }
+    
+    
 }
 
